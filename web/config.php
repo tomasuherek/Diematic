dc590d7ed197fb6e2e6958f67003175cf6dea39c
tomasuherek <uherek.tomas@seznam.cz>
2025-01-25 12:36:58 +0100

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Europe/Paris');
-$modbus_ip='192.168.9.18';
-$modbus_port=20108;
+$modbus_ip='192.168.0.34';
+$modbus_port=501;
 $circuit_defaut="A";
 $language='fr';
 ?>
